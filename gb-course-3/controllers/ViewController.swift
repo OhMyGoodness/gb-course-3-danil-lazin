d5387af7304892d652f6be990bd7dd5d749b7b45
Danil Lazin <xelnagaaa@gmail.com>
2021-12-10 00:09:03 +0700

    lesson-1

---
+++ gb-course-3/controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gb-course-3
+//
+//  Created by Данила Лазин on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ gb-course-3/shared/Session.swift
@@ -0,0 +1,13 @@
+//
+// Created by Данила Лазин on 10.12.2021.
+//
+
+import SwiftUI
+
+class Session {
+    public static let shared = Session()
+
+    var tokenI: String?
+
+    var userId: Int?
+}
